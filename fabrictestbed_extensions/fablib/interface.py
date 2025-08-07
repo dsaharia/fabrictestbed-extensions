c3a65a36979217d9b3fba2d10686bf2ba63d6cc3
Komal Thareja <kthare10@renci.org>
2025-08-07 19:39:11 -0400

    fix get interface

--- fabrictestbed_extensions/fablib/interface.py
+++ fabrictestbed_extensions/fablib/interface.py
@@ -1195,6 +1195,7 @@ class Interface:
 
         :return: None
         """
+        self.config_vlan_iface()
         network = self.get_network()
         if not network:
             logging.info(
