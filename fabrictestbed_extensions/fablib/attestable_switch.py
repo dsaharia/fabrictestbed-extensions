6dcb703ff7c4e2e42e97c72f17d888ccbdbfce1e
Alexander Wolosewicz <112141084+awolosewicz@users.noreply.github.com>
2025-08-27 17:47:22 -0500

    Fix start_switch result

--- fabrictestbed_extensions/fablib/attestable_switch.py
+++ fabrictestbed_extensions/fablib/attestable_switch.py
@@ -731,7 +731,7 @@ V1Switch(
 
         result = None
 
-        if stderr:
+        if stderr and len(stderr) > 0:
             result = False
         else:
             cfg_update.append(self.prep_switch_config_update("Running", True))
